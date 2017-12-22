d48f326a4e718c98f11981e6783dd3311fd54433
lyinch <lyinch@lyinch.com>
2017-12-22 23:12:05 +0100

    camera and glass referection

--- InAWeekend/camera.h
+++ InAWeekend/camera.h
@@ -6,21 +6,42 @@
 #define INAWEEKEND_CAMERA_H
 
 #include "ray.h"
+vec3 random_in_unit_disk(){
+    vec3 p;
+    do{
+        p = 2.0*vec3(drand48(),drand48(),0)-vec3(1,1,0);
+    }while (p.squared_length() >= 1.f);
+    return p;
+}
+
 
 class camera{
 public:
-    camera(){
-        lower_left_corner = vec3(-2.f,-1.f,-1.f);
-        horizontal = vec3(4.f,0.f,0.f);
-        vertical = vec3(0.f,2.f,0.f);
-        origin = vec3(0.f,0.f,0.f);
+    camera(vec3 lookfrom, vec3 lookat, vec3 vup, float vfov, float aspect, float aperture, float focus_dist){ //vfov: top to bottom in degrees
+        lens_radius = aperture/2;
+        float theta = (float)(vfov*M_PI/180);
+        float half_height = tanf(theta/2);
+        float half_width = aspect * half_height;
+        origin = lookfrom;
+        w = normalized(lookfrom-lookat);
+        u = normalized(cross(vup,w));
+        v = cross(w,u);
+        lower_left_corner = origin-half_width*focus_dist*u-half_height*focus_dist*v-w*focus_dist;
+        horizontal = 2*half_width*focus_dist*u;
+        vertical = 2*half_height*focus_dist*v;
     }
 
-    ray get_ray(float u, float v){return ray(origin,lower_left_corner+u*horizontal+v*vertical-origin);}
+    ray get_ray(float s, float t){
+        vec3 rd = lens_radius*random_in_unit_disk();
+        vec3 offset = u*rd.x()+v*rd.y();
+        return ray(origin+offset,lower_left_corner+s*horizontal+t*vertical-origin-offset);
+    }
 
     vec3 origin;
     vec3 lower_left_corner;
     vec3 horizontal;
     vec3 vertical;
+    vec3 u,v,w;
+    float lens_radius;
 };
 #endif //INAWEEKEND_CAMERA_H
--- InAWeekend/main.cpp
+++ InAWeekend/main.cpp
@@ -9,6 +9,29 @@
 #include <fstream>
 
 
+hitable *random_scene(){
+    int n = 500;
+    hitable **list = new hitable*[n+1];
+    list[0] = new sphere(vec3(0,-100,0),1000,new lambertian(vec3(0.5,0.5,0.5)));
+    int i = 1;
+    for (int a = -11; a  < 11; ++a) {
+        for (int b = -11; b < 11; ++b) {
+        float choose_mat = drand48();
+            vec3 center(a+0.9*drand48(),0.2,b+0.9*drand48());
+            if((center-vec3(4,0.2,0)).length() > 0.9){
+                if(choose_mat < 0.8) { //we chose diffuse material
+                    list[i++] = new sphere(center,0.2,new lambertian(vec3(drand48()*drand48(),drand48()*drand48(),drand48()*drand48())));
+                }else if(choose_mat < 0.95){ //metal
+                    //list[i++] = new sphere(center,0.2,new metal();
+                }else{ //glass
+
+                }
+            }
+        } 
+    }
+    return new hitable_list(list,i);
+}
+
 vec3 color(const ray& r, hitable *world,int depth){
     hit_record rec;
     if(world -> hit(r,0.001f,MAXFLOAT,rec)){
@@ -28,23 +51,33 @@ vec3 color(const ray& r, hitable *world,int depth){
 }
 
 
+
+
 int main() {
     std::ofstream file;
     file.open("out.ppm");
 
-    int nx = 800;
-    int ny = 400;
+    int nx = 200;
+    int ny = 100;
     int ns = 100;
     file << "P3\n" << nx << " " << ny << "\n255\n";
 
+    /*
     hitable *list[4];
     list[0] = new sphere(vec3(0,0,-1),0.5,new lambertian(vec3(0.8,0.3,0.3)));
     list[1] = new sphere(vec3(0,-100.5f,-1),100,new lambertian(vec3(0.8,0.8,0.0)));
-    list[2] = new sphere(vec3(1,0,-1),0.5,new metal(vec3(0.8,0.6,0.2),0.3));
-    list[3] = new sphere(vec3(-1,0,-1),0.5,new metal(vec3(0.8,0.8,0.8),1.0));
-    hitable *world = new hitable_list(list,4);
+    list[2] = new sphere(vec3(1,0,-1.5f),0.5,new metal(vec3(0.8,0.6,0.2),0.4f));
+    list[3] = new sphere(vec3(-1,0,-1),0.5f,new dielectric(1.5)); //left */
 
-    camera cam;
+    //hitable *world = new hitable_list(list,4);
+    hitable *scene = random_scene();
+
+    vec3 lookfrom(13,2,3);
+    vec3 lookat(0,0,0);
+    //float dist_to_focus = (lookfrom-lookat).length();
+    float dist_to_focus = 10;
+    float aperture = .1f;
+    camera cam(lookfrom,lookat,vec3(0,1,0),20 ,(float)nx/(float)ny,aperture,dist_to_focus);
     auto start = std::chrono::high_resolution_clock::now();
     for (int j = ny-1; j >= 0; --j) {
         for (int i = 0; i < nx; ++i) {
@@ -54,7 +87,7 @@ int main() {
                 float v = (float)(j+drand48())/(float)ny;
                 ray r = cam.get_ray(u,v);
                 vec3 p = r.point_at_parameter(2.0);
-                col += color(r,world,0);
+                col += color(r,scene,0);
             }
 
             col /= float(ns);
@@ -78,8 +111,8 @@ int main() {
     //free(list[3]);
     //free(list[2]);
     //free(list[1]);
-    free(list[0]);
-    free(world);
+    //free(list[0]);
+    //free(world);
 
     return 0;
 }
